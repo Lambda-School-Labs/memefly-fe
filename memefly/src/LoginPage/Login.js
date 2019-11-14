37796d12676e0f210917aa6dbfede92e881ca01b
Alexnadria Supriana <alexa9410@gmail.com>
2019-11-14 15:29:55 -0500

    SMH

---
+++ memefly/src/LoginPage/Login.js
@@ -0,0 +1,114 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+//import { withCookies } from "react-cookie";
+
+import { Container, Row, Col, Input, Button, Alert, Spinner } from "reactstrap";
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: false,
+      credentials: {
+        username: "",
+        password: ""
+      },
+      error: { status: false, message: "" }
+    };
+  }
+
+  login = e => {
+    e.preventDefault();
+    this.setState({
+      ...this.state,
+      isLoading: true
+    });
+    axios
+      .post(
+        "https://memefly.herokuapp.com/api/user/login",
+        this.state.credentials
+      )
+      .then(res => {
+        this.setState({
+          ...this.state,
+          credentials: {
+            username: "",
+            password: ""
+          }
+        });
+        this.state.set("_uid", res.data.token);
+        this.props.history.push("/login");
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({
+          ...this.state,
+          isLoading: false,
+          error: {
+            status: true,
+            message: "Incorrect Info... please try again ; )"
+          }
+        });
+      });
+  };
+
+  handleChange = e => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  render() {
+    return (
+      <Container>
+        <Row
+          style={{ height: "100vh", alignItems: "center" }}
+          className="text-center"
+        >
+          <Col xs={{ order: 2, size: 12 }} md={{ order: 1, size: 6 }}>
+            {this.state.error.status ? (
+              <Alert color="danger">{this.state.error.message}</Alert>
+            ) : null}
+            <form onSubmit={this.login}>
+              <div onClick={this.logout}>
+                <h2>Login to</h2>
+                <h1>MemeFLY</h1>
+              </div>
+              <Input
+                type="text"
+                name="username"
+                placeholder="Username"
+                value={this.state.credentials.username}
+                onChange={this.handleChange}
+              />
+              <br />
+              <Input
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={this.state.credentials.password}
+                onChange={this.handleChange}
+              />
+              <br />
+              {this.state.isLoading ? (
+                <Spinner color="primary" />
+              ) : (
+                <Button color="primary">Login</Button>
+              )}
+            </form>
+            <div>
+              <h3>Register for a account </h3>
+              <Link to="/register">Register</Link>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Login;
---
+++ memefly/src/RegisterPage/Register.js
@@ -0,0 +1,113 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+//import { withCookies } from "react-cookie";
+
+import { Container, Row, Col, Input, Button, Alert, Spinner } from "reactstrap";
+
+class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: false,
+      newSignup: {
+        username: "",
+        email: "",
+        password: ""
+      },
+      error: { status: false, message: "" }
+    };
+  }
+
+  signupHandler = e => {
+    e.preventDefault();
+    this.setState({
+      ...this.state,
+      isLoading: true
+    });
+    axios
+      .post(
+        "https://memefly.herokuapp.com/api/user/register",
+        this.state.newSignup
+      )
+      .then(res => {
+        this.state.set("_uid", res.data.token);
+        this.props.history.push("/");
+      })
+      .catch(error => {
+        console.log(error);
+        this.setState({
+          ...this.state,
+          isLoading: false,
+          error: { status: true, message: "Sorry but That name is taken." }
+        });
+      });
+  };
+
+  onChange = e => {
+    this.setState({
+      newSignup: {
+        ...this.state.newSignup,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  render() {
+    return (
+      <Container>
+        <Row
+          style={{ height: "100vh", alignItems: "center" }}
+          className="text-center"
+        >
+          <Col xs={{ order: 2, size: 12 }} md="6">
+            {this.state.error.status ? (
+              <Alert color="danger"> {this.state.error.message} </Alert>
+            ) : null}
+            <form onSubmit={this.signupHandler}>
+              <div onClick={this.logout}>
+                <h2>Login to</h2>
+                <h1>MemeFLY!</h1>
+              </div>
+              <Input
+                type="text"
+                name="username"
+                placeholder="Username"
+                value={this.username}
+                onChange={this.onChange}
+              />
+              <br />
+              <Input
+                type="text"
+                name="email"
+                placeholder="Email"
+                value={this.email}
+                onChange={this.onChange}
+              />
+              <br />
+              <Input
+                type="password"
+                name="password"
+                placeholder="Password"
+                onChange={this.onChange}
+                value={this.password}
+              />
+              <br />
+              {this.state.isLoading ? (
+                <Spinner color="primary" />
+              ) : (
+                <Button color="primary">Register</Button>
+              )}
+            </form>
+            <div>
+              <h3>Login to account </h3>
+              <Link to="/">Login</Link>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Register;
