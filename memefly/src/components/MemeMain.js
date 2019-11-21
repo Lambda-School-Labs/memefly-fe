1f3da56ba6cf05c8f96e0098a27372c5ee4ab28a
Derek Etman <dereketman@gmail.com>
2019-11-21 08:54:54 -0800

    + Top and bottom text appears on Images - Lindsey + Image is pushed to main when selected + Refactored Card Size - Lindsey
    Co-authored-by: Lindsey Cason <lindseyacason@gmail.com>

--- memefly/src/components/MemeMain.js
+++ memefly/src/components/MemeMain.js
@@ -1,53 +1,91 @@
 import React, { useState, useEffect, Context, useRef } from "react";
 import axios from "axios";
 import DisplayTemplates from "../ImgEditor/DisplayTemplates";
+import {Meme} from "../ImgEditor/Meme";
 
 const MemeMain = () => {
   const [search, setSearch] = useState("");
   const [allMemes, setAllMemes] = useState([]);
-  const [memeData, setMemeData] = useState({url: "https://i.ibb.co/g911kmh/phone-meme.jpg"});
+  const [memeData, setMemeData] = useState({
+    name: "", 
+    url: "https://imgflip.com/s/meme/Batman-Slapping-Robin.jpg"});
   const [rankUrl, setRankUrl] = useState([]);
   const [memeText, setMemeText] = useState("");
-  const [fontSize, setFontSize] = useState(1000);
+  const [fontSize, setFontSize] = useState(45);
+  const [ naturalWidth, setNaturalWidth] = useState(0);
+
+  const [templates, setTemplates] = useState([]);
+  const [template, setTemplate] = useState(null);
 
     const canvas = useRef(null);
 
   // Places Text on image
     function imageOverlay() {
       console.log("Inside Image Overlay");
-
-      console.log("current", canvas.current);
+      console.log(memeData);
       const ctx = canvas.current.getContext("2d");
 
-      ctx.fillStyle = "Red";
-      ctx.strokeStyle = "Black";    
-      var textMeasure = ctx.measureText(memeData.name) //change dynamically
+      var textWidth = ctx.measureText(memeData.name).width; //change dynamically
+      console.log(textWidth);
+      ctx.font = "40px Impact";
+      ////The function below breaks text at blank spaces to wrap on the canvas
+      var topText = "THIS IS THE TOP TEXT. IS THIS TEXT LONG ENOUGH TO TEST?"
+      var bottomText = "THIS IS THE BOTTOM TEXT"
+      // memeData.name;
+      console.log("txt:", topText)
 
-      var test = ctx.fillText(memeData.name, 50, 125);   
+      printAtWordWrap(ctx, topText, 50, 60, 40, (Math.min(400, naturalWidth)) ); //fit to whichever is smaller, 400 or natural width
+      printAtWordWrap(ctx, bottomText, 50, 360, 40, (Math.min(400, naturalWidth)) ); //fit to whichever is smaller, 400 or natural width
 
-
-      ctx.font = `${fontSize}px Impact`;
+    function printAtWordWrap( context , text, x, y, lineHeight, fitWidth)
+      {
+         ctx.fillStyle = "#ffffff";
+         ctx.strokeStyle = "#000000";
+         ctx.lineWidth=2;
+         fitWidth = fitWidth || 0;
     
-      console.log("%c memeData: ", 'background: #222; color: #bada55', memeData.name)
+          if (fitWidth <= 0)
+            {        
+              context.fillText( text, x, y );
+              // context.strokeText( text, x, y );
 
-      // Adjust Font if Width is too big
-      var textWidth=textMeasure.width;//335.83984375  Same every time for "Chicken"
-      // var textHeight=textMeasure.height; //undefined
-      console.log("Text Measurements: ", textWidth); 
+              return;
+            }
+            var words = text.split(' ');
+            var currentLine = 0;
+            var idx = 1;
+            while (words.length > 0 && idx <= words.length)
+          {
+            var str = words.slice(0,idx).join(' ');
+            var w = context.measureText(str).width;
+            if ( w > fitWidth )
+            {
+                if (idx==1)
+                {
+                    idx=2;
+                }
+                context.fillText( words.slice(0,idx-1).join(' '), x, y + (lineHeight*currentLine) );
+                context.strokeText( words.slice(0,idx-1).join(' '), x, y + (lineHeight*currentLine) );
 
-      const textSize=()=>{
-      if(textWidth>max_width){
-        setFontSize(fontSize -200);
-      }else{
-        return
-      }  // then reduce the font size and re-measure
-      }
-      textSize();
-   } 
+                currentLine++;
+                words = words.splice(idx-1);
+                idx = 1;
+                context.strokeText( words.slice(0,idx-1).join(' '), x, y + (lineHeight*currentLine) );
 
+            }
+              else
+              {idx++;}
+           }
+          if  (idx > 0)
+    context.fillText( words.join(' '), x, y + (lineHeight*currentLine) );
+    context.strokeText( words.join(' '), x, y + (lineHeight*currentLine) );
+  }
+      ////END of function that wraps canvas.
 
+      console.log("%c memeData: ", 'background: #222; color: #bada55', memeData.name) 
+     
+   }//End of Image Overlay 
     var image = new Image();
-
     const  max_width = '500px';
     function renderImage(src) {
       var image = new Image();
@@ -59,7 +97,8 @@ const MemeMain = () => {
           image.height *= max_width / image.height;
           image.width = max_width;
         }
-  
+        setNaturalWidth(image.width); //this is to prevent font stretching
+
       // This makes a canvas on top of the image that matches the width and height of the image 
         let ctx = canvas.getContext("2d");
         ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -81,7 +120,6 @@ const MemeMain = () => {
       .get("URL 'INCLUDES' ENDPOINT FOR SEARCH FUNCTION"`${search}`)
       .then(response => {
         setMemeData(response.data.url);
-
         console.log(response);
       })
       .catch(error => {
@@ -106,6 +144,7 @@ const MemeMain = () => {
       .then((res) => {
         console.log(res.data.data.getMemes[0].url)
         setAllMemes(res.data.data.getMemes)
+        setTemplates(res.data.data.getMemes);
       })
 },[])
  
@@ -114,72 +153,8 @@ const MemeMain = () => {
     console.log("ALL MEMES HERE", allMemes[randomNumber]);
     setMemeData(allMemes[randomNumber])
     setTimeout(imageOverlay, 500);
-
   };
 
-    // axios(
-    //   {
-    //     url:
-    //       "https://memefly.herokuapp.com/api/memes?query=%7B%0A%20getMemes%7B%0A%09name%0A%20%20box%0A%20%20url%0A%09%7D%0A%7D",
-    //     method: "post",
-    //     data: {
-    //       query: `query getMemes{name box url}`
-    //     }
-    //   }
-    // )
-    //   .then((res) => {
-    //     // Selects a Random Meme from Web BE and stores the data in memeData 
-    //     let randomNumber = Math.floor(Math.random() * 104);
-    //     setMemeData(res.data.data.getMemes[randomNumber]);
-    //     console.log(memeData);
-    //     //set state of image to generated true
-        
-      
-        // return axios
-        //   .post(
-        //     "http://memeflydsapp-env.cjpgr2xwjn.us-west-2.elasticbeanstalk.com/generate-meme-text?meme_name=Y-U-No",
-        //     {
-        //       headers: {
-        //         "Content-Type": "application/json",
-        //         'Authorization': ''
-        //       }
-        //     }
-        //   )
-        //   .then(res => {
-        //     // setMemeText()
-        //     console.log("DS AXIOS: ", res);
-        //   });
-
-      // })
-      // .catch(error => {
-      //   console.log("error in axios call generate", error);
-      // });
-
-      // async function waitForText(){
-      //   const data = await axios.get("https://memefly.herokuapp.com/api/memes?query=%7B%0A%20getMemes%7B%0A%09name%0A%20%20box%0A%20%20url%0A%09%7D%0A%7D")
-      // }
-  
-
-  //THIS CAN BE USED ONCE WE GET A RANKING SYSTEM IN PLACE
-  useEffect(() => {
-    axios
-      .get(
-        "http://version1.api.memegenerator.net//Generators_Select_ByNew?pageIndex=0&pageSize=12&apiKey=demo"
-      )
-      .then(res => {
-        // console.log(res.data.result);
-        setRankUrl(res.data.result);
-      })
-      .catch(error => {
-        console.log("error in top5", error);
-      });
-  }, []);
-
-  // For each item in the res, check to see if the count is <= 5, if so render an img with src of res.data.url
-  ////////////////////////////////////////////////////////
-  // rankUrl.map(item => {
-  //   console.log(item.imageUrl);
-  // });
 
   return (
     <div className="MainContainer">
@@ -193,18 +168,44 @@ const MemeMain = () => {
             style={{ width: "500px", display: "hidden" }}
           >
             {renderImage(memeData.url)}{" "}
-
           </canvas>
         </div>
 
-        <button onClick={generateMeme} id="mainGenerateButton" >GENERATE MEME</button>
+        <button onClick={generateMeme} id="mainGenerateButton">
+          GENERATE MEME
+        </button>
 
         <>
-        <DisplayTemplates />    
+          <div className="trendingMeme">
+            <div
+              className="templateMemeContainer"
+              
+              >
+              <div className="MidPageNav">
+                <h2> Pick a Template!</h2>
+              </div>
+              
+              <div className="templateMemeGroup">
+                {templates.map(template => {
+                  return (
+                    <>
+                    <Meme
+                      template={template}
+                      onClick={() => {
+                        setMemeData(template);
+                      }}
+                    />
+                    </>
+                  );
+                })}
+              </div>
+              
+            </div>
+          </div>
         </>
-
       </div>
-  </div> )
+    </div>
+  );
   }
 
  export default MemeMain;
