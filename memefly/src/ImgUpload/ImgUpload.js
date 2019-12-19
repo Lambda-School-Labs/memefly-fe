3c0809337af85597f253cf98742febec4c288aa3
Lindsey Cason <lindseyacason@gmail.com>
2019-12-19 13:22:39 -0600

    added blob for uploaded image URL

--- memefly/src/ImgUpload/ImgUpload.js
+++ memefly/src/ImgUpload/ImgUpload.js
@@ -14,14 +14,17 @@ console.log(memeURL)
         reader.addEventListener("load", function () {
           preview.src = reader.result;
           console.log("base64?", preview.src)
+          const uploadedImageURL = URL.createObjectURL(file)
+          //include blob -> ex: blob:http://localhost:3002/215ed49c-5e30-408f-9da5-c047078ec3b2
+          console.log(uploadedImageURL);
           // const base64 = btoa(preview.src);
           // const decoded = atob(base64);
+
         }, false);
 
         if (file) {
           reader.readAsDataURL(file);
-          console.log("url", preview.src)
-          console.log(file)
+          console.log("entire uploaded file", file)
         }
       }
 
--- memefly/src/store/reducers/memeReducer.js
+++ memefly/src/store/reducers/memeReducer.js
@@ -45,26 +45,8 @@ export const uploadImageReducer = (state = initialState.meme.meme_url, action)=>
     switch(action.type){
         case UPLOAD_IMAGE_START:
             return{
-                payload: 
-                function previewFile() {
-                    var preview = document.getElementById('imagePreview');
-                    var file    = document.querySelector('input[type=file]').files[0];
-                    var reader  = new FileReader();
-                  
-                    reader.addEventListener("load", function () {
-                      preview.src = reader.result;
-                      console.log("base64?", preview.src)
-                      // const base64 = btoa(preview.src);
-                      // const decoded = atob(base64);
-                    }, false);
-            
-                    if (file) {
-                      reader.readAsDataURL(file);
-                      console.log("url", preview.src)
-                      console.log(file)
-                    }
-                  }
-            
+                payload: ""
+                
             }
             default:
                 return state;
