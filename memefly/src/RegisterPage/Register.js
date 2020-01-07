2fb6168b093d22ed8b3297096f1c0d88d3d50132
Lindsey Cason <lindseyacason@gmail.com>
2020-01-07 10:12:28 -0600

    updated links to axios

--- memefly/src/RegisterPage/Register.js
+++ memefly/src/RegisterPage/Register.js
@@ -22,8 +22,8 @@ const Register = () => {
 		try {
 			let config = {
 				method: "POST",
-				// url: "http://memefly.herokuapp.com/api/accounts",
-				url:"https://localhost:5000/api/accounts",
+				url: "http://memefly.herokuapp.com/api/accounts",
+				// url:"https://localhost:5000/api/accounts",
 				data: {
 					query: `
 	              mutation{
