2580ff405d330db88179f6de49fb41e9eb04abc6
Lindsey Cason <lindseyacason@gmail.com>
2020-01-09 12:24:26 -0600

    all caps font per request

--- memefly/src/ImgEditor/ImgContainer.js
+++ memefly/src/ImgEditor/ImgContainer.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState, useRef} from "react";
 import { fabric } from "fabric";
 import { connect } from "react-redux";
 import domtoimage from 'dom-to-image';
+import html2canvas from 'html2canvas';
 
 function ImgContainer({meme_url, generated_meme_texts}) {
 
@@ -25,9 +26,11 @@ function ImgContainer({meme_url, generated_meme_texts}) {
 		top:16,
 		left:20,
 		width: textWidth,
-		fontFamily:'impact',
+		fontFamily:'Alegreya Sans SC',
 		fill:'white',
-		stroke: 'black'
+		stroke: 'black',
+		fontWeight: 900,
+		textTransform: 'uppercase'
 	});
 
 	// console.log("memeURL", meme_url);	
@@ -102,7 +105,8 @@ function ImgContainer({meme_url, generated_meme_texts}) {
 		
 			
 		memeImg.src = tempImg
-
+		// const upperText = text2.toUpperCase();
+		let upperText = (text2.text.toUpperCase())
 		canvas.add(text2);
 		console.log('mememimg', memeImg)
 
@@ -129,11 +133,11 @@ function ImgContainer({meme_url, generated_meme_texts}) {
 		}).catch(function(error){
 		console.log('oops, something went wrong!', error)
 	});
-			// html2canvas(document.getElementById('canvasContainer'), { allowTaint: true }).then(function(canvas){
-			// 	document.body.appendChild(canvas);
-			// 	console.log(canvas.toDataURL())
-				//
-			// })
+			html2canvas(document.getElementById('d')).then(function(canvas){
+				document.body.appendChild(canvas);
+				console.log("HELLO", canvas.toDataURL())
+				
+			})
 		}
 return (
 		<div>
